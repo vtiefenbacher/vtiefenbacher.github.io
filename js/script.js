cae2731d9e8352bea792c0832aa2adc7f238a02d
friedrich15 <emdo2000@gmail.com>
2016-02-04 16:12:08 +0100

    Added mobile menu.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
-  $('#home-link').click(function(){
-    $('.active-sub').hide();
-    $('.active-sub').removeClass('active-sub');
-    $('.active').removeClass('active');
-  })
-
-  $('.menu-item').click(function(e){
-    $('.active-sub').hide();
-    $('.active-sub').removeClass('active-sub');
-    $('.active').removeClass('active');
-    $(this).children('.submenu').addClass('active-sub').show();
-    $(this).children('a').addClass('active');
+  // $('#home-link').click(function(){
+  //   $('.active-sub').hide();
+  //   $('.active-sub').removeClass('active-sub');
+  //   $('.active').removeClass('active');
+  // })
+  //
+  // $('.menu-item').click(function(e){
+  //   $('.active-sub').hide();
+  //   $('.active-sub').removeClass('active-sub');
+  //   $('.active').removeClass('active');
+  //   $(this).children('.submenu').addClass('active-sub').show();
+  //   $(this).children('a').addClass('active');
+  // });
+  $('#side-menu').slicknav({
+    label: '',
+    prependTo: '#home-link-mobile'
   });
 });
