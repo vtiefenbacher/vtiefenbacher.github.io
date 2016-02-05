2d3f821b40bb8650ba104f74bfb901a19c1d4e7b
friedrich15 <emdo2000@gmail.com>
2016-02-05 21:41:22 +0100

    Images to fade in after loading.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// $("#loadingDiv").show();
-
 
 $(document).ready(function(){
   $('#side-menu').slicknav({
     label: '',
     prependTo: '#home-link-mobile'
   });
+  $('.pin img').load(function() {}).each(function(i, e){
+    $(e).closest('.pin').addClass('loaded');
+  })
 
-  
 });
 
 
